ba1b2b84034ff6c1d471a0630a1a77f3eecefafb
Kevin Liu <kliu0932@gmail.com>
2023-04-06 21:12:05 -0400

    responsive changes

--- src/whats-happening/index.js
+++ src/whats-happening/index.js
@@ -3,11 +3,11 @@ import "./index.css";
 const WhatsHappening = ({ currentUser }) => {
     return (
         <div className="sf-padding-20 row">
-            <div className="col-1">
+            <div className="col-2 col-md-1">
                 <img className='rounded-circle' src={`./images/catjam.jpg`} height='50px' width='50px' />
             </div>
-            <div className="ms-2 col-10">
-                <textarea className="form-control" placeholder="What's happening?"></textarea>
+            <div className="ms-2 col-9 col-md-10">
+                <textarea className="sf-text-area-no-resize form-control" placeholder="What's happening?"></textarea>
             </div>
         </div>
     )
