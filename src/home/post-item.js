4510c8dd0a246d4c49fb8d2c8b273b74ff81d6e4
faosldkjfhf <60448325+faosldkjfhf@users.noreply.github.com>
2023-04-07 23:34:31 -0400

    added ability to delete posts

--- src/home/post-item.js
+++ src/home/post-item.js
@@ -1,8 +1,15 @@
 import '../index.css'
 import './index.css'
 import PostStats from './post-stats';
+import { useDispatch } from 'react-redux';
+import { deletePostThunk } from '../services/posts-thunk';
 
 const PostItem = ({ post }) => {
+    const dispatch = useDispatch();
+    const deletePostHandler = (id) => {
+        dispatch(deletePostThunk(id));
+    }
+
     return (
         <div className="sf-home-post-border sf-tertiary">
             <div className="row px-3">
@@ -12,6 +19,8 @@ const PostItem = ({ post }) => {
                     </div>
                 </div>
                 <span className="sf-font-bold col-11 col-lg-11">
+                    <i className="fa-solid fa-x float-end"
+                        onClick={() => deletePostHandler(post._id)}></i>
                     {post.userName} <span class="fa-solid fa-circle-check sf-accent"></span>
                     <span className="sf-font-normal"> @{post.handle}</span>
                     <div className="sf-font-normal">
--- src/whats-happening/index.js
+++ src/whats-happening/index.js
@@ -1,19 +1,33 @@
 import "./index.css";
+import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { createPostThunk } from "../services/posts-thunk";
 
 const WhatsHappening = () => {
+    const [post, setPost] = useState("");
+    const dispatch = useDispatch();
+    const postClickHandler = () => {
+        const newPost = {
+            post
+        }
+        dispatch(createPostThunk(newPost));
+    }
     return (
         <div className="sf-padding-20 row">
             <div className="col-2 col-md-1">
                 <img className='rounded-circle' src={`./images/catjam.jpg`} height='50px' width='50px' />
             </div>
             <div className="ms-2 col-9 col-md-10">
-                <textarea className="sf-text-area-no-resize form-control" placeholder="What's happening?"></textarea>
+                <textarea onChange={(e) => {
+                    setPost(e.target.value);
+                }} className="sf-text-area-no-resize form-control" placeholder="What's happening?"></textarea>
             </div>
             <div className="sf-center sf-frame">
-                <button className="mt-2 sf-custom-btn sf-btn-1">
+                <button onClick={postClickHandler} className="mt-2 sf-custom-btn sf-btn-1">
                     <span>Post</span>
                 </button>
             </div>
+            {post}
         </div>
     )
 }
