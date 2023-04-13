112c9f9174d3df153e5e0afdb2362833bc48ea52
Kevin Liu <kliu0932@gmail.com>
2023-04-13 18:51:11 -0400

    pulling

--- src/edit-profile/index.js
+++ src/edit-profile/index.js
@@ -1,15 +1,20 @@
 import { useSelector } from "react-redux"
+import { useState } from "react";
 import "../index.css";
 
 const EditProfileScreen = () => {
     const { currentUser } = useSelector(state => state.user);
+    const [firstName, setFirstName] = useState(currentUser.firstName);
+    const [lastName, setLastName] = useState(currentUser.lastName);
     return (
         <div>
             <h1 className="sf-secondary">Edit Profile</h1>
             {currentUser &&
                 <div>
                     <label for="firstName">First Name</label>
-                    <input className="form-control" id="firstName"></input>
+                    <input onChange={(e) => setFirstName(e.target.value)} className="form-control" id="firstName" value={lastName}></input>
+                    <label for="lastName">Last Name</label>
+                    <input onChange={(e) => setFirstName(e.target.value)} className="form-control" id="lastName" value={lastName}></input>
                 </div>
             }
         </div>
