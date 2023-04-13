3d3a4cae9bcff9a39d630ef535ba65c8de63454e
Daniel Wu <danielwu.toucan@gmail.com>
2023-04-13 18:41:37 -0400

    navbar ui updates

--- src/nav-bar/mobile.js
+++ src/nav-bar/mobile.js
@@ -2,6 +2,8 @@ import MobileItem from "./mobile-item";
 import sidebar from "./sidebar.json";
 import '../index.css';
 import "./index.css";
+import { Link } from "react-router-dom";
+
 
 const MobileNavBar = ({ active = 'home' }) => {
     return (
@@ -12,8 +14,12 @@ const MobileNavBar = ({ active = 'home' }) => {
                         <MobileItem item={sidebarItem} active={active} />
                     ))}
                 </div>
-                <div className="col-2 text-center">
-                    <img src="./images/catjam.jpg" className="sf-mobile-nav-pfp" alt="profile" />
+                <div className="col-2 text-center d-inline-flex align-items-center justify-content-center">
+                    <Link to="/login">
+                        <div className="sf-mobile-nav-pfp-container">
+                            <img src="./images/catjam.jpg" className="sf-mobile-nav-pfp" alt="profile" />
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
