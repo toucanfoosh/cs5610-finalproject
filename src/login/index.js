32df24cc4e7edc08ce681b83e87264191981f7e6
Daniel Wu <danielwu.toucan@gmail.com>
2023-04-23 02:50:03 -0400

    login

--- src/login/index.js
+++ src/login/index.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { profileThunk, loginThunk } from "../services/user-thunk";
 import FancyButton from "../FancyButton/button";
+import './index.css';
+import '../index.css';
 
 const Login = () => {
     const { currentUser } = useSelector((state) => state.user);
@@ -35,42 +37,58 @@ const Login = () => {
     }, [currentUser]);
 
     return (
-        <div>
-            <h1>
-                Login Screen
-            </h1>
+        <div className="sf-w-100 sf-vh-100 d-flex flex-column justify-content-center align-items-center">
+            <div className="d-flex flex-column align-items-center">
+                <div className="d-flex flex-column align-items-start pb-4">
+                    <label for="username">Username</label>
+                    <input
+                        id="username"
+                        className="sf-form-control fs-3"
+                        type="text"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                handleLogin();
+                            }
+                        }}
+                    />
+                </div>
+                <div className="d-flex flex-column align-items-start">
+                    <label for="password" >Password</label>
+                    <input
+                        id="password"
+                        className="sf-form-control fs-3"
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                handleLogin();
+                            }
+                        }}
+                    />
+                </div>
+            </div>
+            <div className="d-flex flex-column align-items-center p-5 sf-w-100">
+                <div className="sf-login-button">
+                    <FancyButton onclick={handleLogin} text="Login" />
+                </div>
+                <div className="p-4">
+                    Don't have an account? <a href="#" className="sf-accent sf-underline-hover" onClick={handleRegister}>Register</a>
+                </div>
+            </div>
+
             {error && (
-                <div className="alert alert-danger" role="alert">
-                    {error}
+                <div className="position-absolute sf-liked">
+                    Sorry, we couldn't find that account. 
                 </div>
             )}
-            <div>
-                <label>Username</label>
-                <br />
-                <input
-                    className="form-control"
-                    type="text"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                />
-            </div>
-            <div>
-                <label>Password</label>
-                <input
-                    className="form-control"
-                    type="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-            </div>
-            <FancyButton onclick={handleLogin} text="Login" />
-            <FancyButton onclick={handleRegister} text="Register" />
-
-            {currentUser && (
+            {/* {currentUser && (
                 <div>
                     <h1>Welcome {currentUser.username}</h1>
                 </div>
-            )}
+            )} */}
         </div>
     );
 };
