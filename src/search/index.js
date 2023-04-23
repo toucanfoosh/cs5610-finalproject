4c8443017d47b2c3780d5c9370c6a40f586e6965
faosldkjfhf <60448325+faosldkjfhf@users.noreply.github.com>
2023-04-22 21:22:48 -0400

    review average displays on search

--- src/search/index.js
+++ src/search/index.js
@@ -9,13 +9,14 @@ import "./index.css";
 import "../index.css";
 import { Link } from "react-router-dom";
 import LoadingIcon from "../loading-icon";
+import { findReviewsByAlbumThunk } from "../services/reviews-thunk";
 
 const SearchScreen = () => {
     const { searchTerm } = useParams();
     const [search, setSearch] = useState(searchTerm || "");
     const [accessToken, setAccessToken] = useState("");
     const [searchResults, setSearchResults] = useState({});
-    const [searchResultItems, setSearchResultItems] = useState([]);
+    const [searchResultItems, setSearchResultItems] = useStateWithCallbackLazy([]);
     const [waiting, setWaiting] = useState(false);
     const [offset, setOffset] = useStateWithCallbackLazy(0);
     const navigate = useNavigate();
@@ -32,8 +33,48 @@ const SearchScreen = () => {
         }
     }, [search]);
 
+    const handleAverageReviews = async (id) => {
+        const response = await dispatch(findReviewsByAlbumThunk(id));
+        const reviews = response.payload;
 
-    const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+        let totalScore = 0;
+        if (reviews.length > 0) {
+            for (const review of reviews) {
+                totalScore += review.score;
+            }
+
+            totalScore = totalScore / reviews.length;
+            return {
+                score: totalScore,
+                numReviews: reviews.length
+            }
+        }
+        else {
+            return {
+                score: "-",
+                numReviews: "(0)"
+            }
+        }
+    }
+
+    const handleHoldScore = async (items) => {
+        const reviews = await Promise.all(items.map(item => {
+            return handleAverageReviews(item.id)
+        }));
+        let count = -1;
+
+        const newSearchResultItems = items.map(item => {
+            count++;
+            return {
+                ...item,
+                score: reviews[count].score,
+                numReviews: reviews[count].numReviews
+            }
+        })
+
+        console.log(newSearchResultItems);
+        setSearchResultItems(newSearchResultItems);
+    }
 
     const searchSpotify = async (offsetNum) => {
         window.localStorage.setItem("token", accessToken);
@@ -59,12 +100,11 @@ const SearchScreen = () => {
             setSearchResultItems(searchResultItems => [
                 ...searchResultItems,
                 ...results.data.albums.items
-            ])
-            console.log(searchResultItems);
+            ], result => handleHoldScore(result));
         }
         else {
             setSearchResults(results.data.albums);
-            setSearchResultItems(results.data.albums.items);
+            setSearchResultItems(results.data.albums.items, result => handleHoldScore(result));
         }
 
         navigate(`/search/${search}`);
@@ -107,35 +147,36 @@ const SearchScreen = () => {
                     {
                         !waiting &&
                         searchResults.items &&
-                        searchResultItems.map(result =>
-                            <Link className="sf-no-text-decor" to={`/search/album/${result.id}`}>
-                                <div className="p-1">
-                                    <div className="sf-result-container sf-result-hover d-flex align-items-center">
-                                        <div className="sf-result-body-container d-flex sf-w-100">
-                                            <div className="col-9 d-flex">
-                                                <img src={result.images[0].url} className="sf-result-img p-2" />
-                                                <div className="ps-3 sf-flex-col justify-content-center align-items-start text-truncate sf-result-body">
-                                                    <div className="sf-w-100 sf-secondary sf-text-bold text-truncate">{result.name}</div>
-                                                    <div className="sf-tertiary text-truncate">{result.artists[0].name}</div>
+                        searchResultItems.map(result => {
+                            return (
+                                <Link className="sf-no-text-decor" to={`/search/album/${result.id}`}>
+                                    <div className="p-1">
+                                        <div className="sf-result-container sf-result-hover d-flex align-items-center">
+                                            <div className="sf-result-body-container d-flex sf-w-100">
+                                                <div className="col-9 d-flex">
+                                                    <img src={result.images[0].url} className="sf-result-img p-2" />
+                                                    <div className="ps-3 sf-flex-col justify-content-center align-items-start text-truncate sf-result-body">
+                                                        <div className="sf-w-100 sf-secondary sf-text-bold text-truncate">{result.name}</div>
+                                                        <div className="sf-tertiary text-truncate">{result.artists[0].name}</div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div className="sf-flex-col col-3 sf-review-stats d-flex justify-content-center align-items-end">
-                                                <div className="sf-flex-col pe-3 text-truncate">
-                                                    <div className="sf-secondary sf-text-bold">Rating: #/5</div>
-                                                    <div className="sf-tertiary"># reviews</div>
+                                                <div className="sf-flex-col col-3 sf-review-stats d-flex justify-content-center align-items-end">
+                                                    <div className="sf-flex-col pe-3 text-truncate">
+                                                        <div className="sf-secondary sf-text-bold">Rating: {result.score !== "-" ? (("" + result.score).length > 4 ? ("" + result.score).slice(0, 4) : result.score) : result.score}/5</div>
+                                                        <div className="sf-tertiary">{result.numReviews} reviews</div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </Link>
-
-                        )
+                                </Link>
+                            )
+                        })
                     }
                 </div>
                 <div className="d-flex justify-content-center">
                     {
-                        !waiting && searchResults.items && searchResultItems.length > 0 &&
+                        !waiting && searchResults.items && searchResults.items.length > 0 &&
                         <div className="sf-results-button-container m-3">
                             <FancyButton onclick={() => {
                                 setOffset(offset + 8, result => searchSpotify(result));
@@ -143,7 +184,7 @@ const SearchScreen = () => {
                         </div>
                     }
                     {
-                        searchResults.items && searchResultItems.length === 0 &&
+                        searchResults.items && searchResults.items.length === 0 &&
                         <div className="text-center">
                             No results found
                         </div>
