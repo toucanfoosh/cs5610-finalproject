a6ad615770de34bae1fed27f4717b99d31cd8d56
Daniel Wu <danielwu.toucan@gmail.com>
2023-04-27 14:34:39 -0400

    styling fixes

--- src/edit-profile/index.js
+++ src/edit-profile/index.js
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState } from "react";
 import "../index.css";
+import "./index.css";
 import FancyButton from "../FancyButton/button";
 import { useNavigate } from "react-router";
 import { profileThunk, updateUserThunk } from "../services/user-thunk";
+import BackButton from "../back-button";
 
 const EditProfileScreen = () => {
     const { currentUser } = useSelector(state => state.user);
@@ -17,28 +19,35 @@ const EditProfileScreen = () => {
     console.log(user);
     return (
         <div>
-            <h1 className="sf-secondary">Edit Profile</h1>
+            <BackButton path="/profile" />
+            <h1 className="sf-secondary p-3 pb-0 pt-5">Edit Profile</h1>
             {user &&
-                <div>
+                <div className="d-flex flex-column p-3">
                     <label for="firstName">First Name</label>
                     <input onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-                        className="form-control" id="firstName" value={user.firstName}></input>
+                        className="sf-form-control sf-edit-h" id="firstName" value={user.firstName}></input>
                     <label for="lastName">Last Name</label>
                     <input onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-                        className="form-control" id="lastName" value={user.lastName}></input>
+                        className="sf-form-control sf-edit-h" id="lastName" value={user.lastName}></input>
                     <label for="handle">Handle</label>
                     <input onChange={(e) => setUser({ ...user, handle: e.target.value })}
-                        className="form-control" id="handle" value={user.handle}></input>
+                        className="sf-form-control sf-edit-h" id="handle" value={user.handle}></input>
                     <label for="bio">Bio</label>
                     <input onChange={(e) => setUser({ ...user, bio: e.target.value })}
-                        className="form-control" id="bio" value={user.bio}></input>
-                    <FancyButton onclick={async () => {
-                        await dispatch(updateUserThunk(user))
-                        navigate("/profile");
-                    }} text="Save" />
-                    <FancyButton onclick={() => {
-                        navigate("/profile");
-                    }} text="Cancel" />
+                        className="sf-form-control sf-edit-h" id="bio" value={user.bio}></input>
+                    <div className="row pt-5 d-flex justify-content-around">
+                        <div className="col sf-edit-btn">
+                            <FancyButton onclick={async () => {
+                                await dispatch(updateUserThunk(user))
+                                navigate("/profile");
+                            }} text="Save" />
+                        </div>
+                        <div className="col sf-edit-btn">
+                            <FancyButton onclick={() => {
+                                navigate("/profile");
+                            }} text="Cancel" />
+                        </div>
+                    </div>
                 </div>
             }
         </div>
